9e4170e48eb7bf65b5241b28beb7d95f989818a1
unknown <alexmitrica40@gmail.com>
2025-01-28 20:43:33 +0200

    primele-modele

---
+++ CourtConnect/StartPackage/Database/CourtConnectDbContext.cs
@@ -0,0 +1,34 @@
+﻿using CourtConnect.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace CourtConnect.StartPackage.Database
+{
+    public class CourtConnectDbContext :DbContext
+    {
+        public CourtConnectDbContext(DbContextOptions<CourtConnectDbContext> options)
+       : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Level> Levels { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            #region Relationship User
+            modelBuilder.Entity<User>()
+                .HasOne(b => b.Level) 
+                .WithMany(a => a.Users) 
+                .HasForeignKey(b => b.LevelId);
+
+            modelBuilder.Entity<User>()
+               .HasOne(b => b.Club)
+               .WithMany(a => a.Users)
+               .HasForeignKey(b => b.ClubId);
+            #endregion
+
+        }
+    }
+}
